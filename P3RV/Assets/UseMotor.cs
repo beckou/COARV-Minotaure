d961e92fbd4868c7ea7cbf73f76bd99564a6fcf9
Sperce <octave@debrich.net>
2019-02-15 11:39:06 +0100

    Ajout d'un script qui ouvre la bouche du crane quand on appuie sur la barre d'espace

---
+++ P3RV/Assets/UseMotor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class UseMotor : MonoBehaviour
+{
+    
+    public Rigidbody Skull;
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("Wallah");
+        Skull = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Skull.AddForce(transform.forward * 25, ForceMode.VelocityChange);
+        }
+    }
+}
