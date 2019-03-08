63012144bcad66944f87a1df1b3cda0fde4c27f3
Sperce <octave@debrich.net>
2019-03-08 10:52:15 +0100

    briquet qui fonctionne par clic, seul souci la flamme est de travers

--- Assets/devotid/Lighter/LighterBehavio.cs
+++ Assets/devotid/Lighter/LighterBehavio.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class LighterBehavio : MonoBehaviour
 {
+    public GameObject Lighter;
+    public GameObject Flamme;
+    bool flammeAllumee=false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,9 +16,23 @@ public class LighterBehavio : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        GameObject flamme;
         if (Input.GetMouseButtonDown(0))
         {
-            Debug.Log("Pressed primary button.");
+
+            if (!flammeAllumee)
+            {
+                Vector3 hauteurFlamme = new Vector3(0, 0.18f, 0);
+                Quaternion rotationFlamme = new Quaternion(-0.5f, 0, 0, 0);
+                flamme = Instantiate(Flamme, Lighter.transform.position + hauteurFlamme, Lighter.transform.rotation * rotationFlamme);
+                flammeAllumee = true;
+            }
+
+            else
+            {
+                Destroy(GameObject.Find("Lighter_Flame(Clone)"));
+                flammeAllumee = false;
+            }
         }
     }
 }
