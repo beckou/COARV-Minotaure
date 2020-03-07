7b657f1b2d4f3f2326933da56546030f78b121a0
saberkane2018 <salwa9aberkane@gmail.com>
2020-03-07 12:53:12 +0100

    le highlighter

---
+++ Assets/Shader/HighlightManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightManager : MonoBehaviour
+{
+    private GameObject obj1;
+    private GameObject obj2;
+
+    public Material sh;
+    // Start is called before the first frame update
+    void Start()
+    {
+        obj1=GameObject.Find("Cube");
+        obj2=GameObject.Find("Sp");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+          obj1.GetComponent<Renderer> ().material = sh;
+          obj2.GetComponent<Renderer> ().material = sh;
+        }
+    }
+}
