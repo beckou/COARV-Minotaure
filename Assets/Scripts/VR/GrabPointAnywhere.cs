1277b7dead3e008a9ebb46e94f1aaf2b5b5608c2
BaptisteMaillard <baptiste.maillard@eleves.ec-nantes.fr>
2022-03-10 09:56:22 +0100

    Le script GrabPointAnywhere est fonctionnel

--- Assets/Scripts/VR/GrabPointAnywhere.cs
+++ Assets/Scripts/VR/GrabPointAnywhere.cs
@@ -4,16 +4,30 @@ using UnityEngine;
 
 public class GrabPointAnywhere : MonoBehaviour
 {
-    public GameObject main;
+    public GameObject mainD;
+    public GameObject mainG;
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other == main.GetComponent<SphereCollider>())
+        if (other == mainD.GetComponent<SphereCollider>())
         {
+            Debug.Log("Détecté");
             GameObject fils = gameObject.transform.GetChild(0).gameObject;
-            Vector3 positionMainRefObjet = fils.transform.InverseTransformPoint(main.transform.position);
+            Vector3 positionMainRefObjet = gameObject.transform.InverseTransformPoint(mainD.transform.position);
+            Quaternion RotationMain = mainD.transform.rotation;
             Debug.Log(positionMainRefObjet);
             fils.transform.localPosition = positionMainRefObjet;
+            fils.transform.rotation = RotationMain;
+        }
+        if (other == mainG.GetComponent<SphereCollider>())
+        {
+            Debug.Log("Détecté");
+            GameObject fils = gameObject.transform.GetChild(0).gameObject;
+            Vector3 positionMainRefObjet = gameObject.transform.InverseTransformPoint(mainG.transform.position);
+            Quaternion RotationMain = mainG.transform.rotation;
+            Debug.Log(positionMainRefObjet);
+            fils.transform.localPosition = positionMainRefObjet;
+            fils.transform.rotation = RotationMain;
         }
     }
 }
