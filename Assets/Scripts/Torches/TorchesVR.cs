83a7f18795fdf882157fd25b40660833e4bbc748
lCommunier <lucas.communier@eleves.ec-nantes.fr>
2022-03-25 08:48:00 +0100

    On peut désormais allumer et éteindre les torches grâce à un rayon

--- Assets/Scripts/Torches/TorchesVR.cs
+++ Assets/Scripts/Torches/TorchesVR.cs
@@ -8,9 +8,12 @@ public class TorchesVR : MonoBehaviour
     public GameObject torchesEnigme;
     public GameObject rayG;
     public bool complete = false;
+    public float tolerance = 0.4f;
 
-    private GameObject[] torches;
-    private GameObject[] tousLesTrucsADesactiver;
+    private GameObject[] torches = new GameObject[4];
+    private GameObject[] tousLesTrucsADesactiver = new GameObject[8];
+    private bool[] etattorche = new bool[4];
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,19 +22,51 @@ public class TorchesVR : MonoBehaviour
             torches[i] = torchesEnigme.transform.GetChild(i).gameObject;
             tousLesTrucsADesactiver[2 * i] = torches[i].transform.GetChild(0).gameObject;
             tousLesTrucsADesactiver[2 * i + 1] = torches[i].transform.GetChild(1).gameObject;
+            etattorche[i] = false;
         }
-        for (int i = 0; i < 8; i++)
+        for (int i = 0; i < 4; i++)
         {
-            tousLesTrucsADesactiver[i].SetActive(false); //ça marche pas pour l'instant ?
+            tousLesTrucsADesactiver[2*i].SetActive(etattorche[i]);
+            tousLesTrucsADesactiver[2*i + 1].SetActive(etattorche[i]);
         }
     }
 
     // Update is called once per frame
     void Update()
     {
-        rayG.GetComponent<XRRayInteractor>().TryGetHitInfo(out Vector3 v, out Vector3 n, out int i, out bool valid);
-        Debug.Log(v);
-        Debug.Log(valid);
-        //On calcule le vecteur torche - player et le vecteur ray hit - player, produit scalaire avec une valeur minimale d'acceptation
+        //Condition pour savoir si le rayon sort
+        XRController manette = rayG.GetComponent<XRController>();
+        if (manette.selectInteractionState.active)
+        {
+            //On récupère le point d'impact
+            rayG.GetComponent<XRRayInteractor>().TryGetHitInfo(out Vector3 v, out Vector3 n, out int i, out bool valid);
+
+            Vector3[] playertoColliders = new Vector3[4];
+
+            //On calcule le vecteur qui va du joueur au point d'impact
+            Vector3 playertov = v - rayG.transform.position;
+
+            //On calcule les vecteurs qui vont du joueur aux points d'impacts
+            for (int j = 0; j < 4; j++)
+            {
+                playertoColliders[j] = torches[j].transform.TransformPoint(torches[j].GetComponent<SphereCollider>().center) - rayG.transform.position;
+            }
+
+            for (int j = 0; j < 4; j++)
+            {
+                //Produit scalaire entre les deux vecteurs pour chaque collider puis on récupère le cosinus de l'angle entre les deux vecteurs
+                float scalar = Vector3.Dot(playertov, playertoColliders[j]);
+                float cos = scalar / (Vector3.Magnitude(playertov) * Vector3.Magnitude(playertoColliders[j]));
+                Debug.Log(scalar);
+
+                //Activation ou désactivation des torches si on considère qu'on Hit
+                if (Mathf.Abs(cos - 1) < tolerance)
+                {
+                    tousLesTrucsADesactiver[2 * j].SetActive(etattorche[j]);
+                    tousLesTrucsADesactiver[2 * j + 1].SetActive(etattorche[j]);
+                    etattorche[j] = !etattorche[j];
+                }
+            }
+        }
     }
 }
