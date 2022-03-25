f537b9cf0240a696881dd7a884b170aac1ec1c73
lCommunier <lucas.communier@eleves.ec-nantes.fr>
2022-03-25 10:34:02 +0100

    merge avec TableInte raction

--- Assets/Scripts/eyeEnigma/InsertEye.cs
+++ Assets/Scripts/eyeEnigma/InsertEye.cs
@@ -16,15 +16,11 @@ public class InsertEye : MonoBehaviour
     {
         if (col.gameObject == MissingEye)
         {
-            float x = FixedEye.transform.localPosition.x;
-            float y = FixedEye.transform.localPosition.y;
-            float z = FixedEye.transform.localPosition.z;
-            MissingEye.transform.localPosition = new Vector3(-x, y, z);
             MissingEye.transform.localRotation = FixedEye.transform.localRotation;
             MissingEye.transform.localScale = FixedEye.transform.localScale;
             //Eye.transform.RotateAround(Relique.transform.position, Vector3.up, 180);
-            MissingEye.transform.SetParent(this.transform); // il retrouve son père d'origine
             col.gameObject.GetComponent<Rigidbody>().isKinematic = true;
+            col.gameObject.transform.position = gameObject.transform.TransformPoint(gameObject.GetComponent<SphereCollider>().center);
             goal = true;
             XRGrabInteractable grabEye = MissingEye.GetComponent<XRGrabInteractable>();
             grabEye.enabled = false;
