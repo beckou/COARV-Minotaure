129635ed61a3d4ab4e9324fd47462fbb28fbc179
lcommun <lucas.communier@eleves.ec-nantes.fr>
2022-03-25 12:21:20 +0100

    Ajout du poème
    Ajout du poème et des scripts

---
+++ Assets/Scripts/Poem/Open.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Open : MonoBehaviour
+{
+    public GameObject mainDroite;
+    public GameObject mainGauche;
+    public bool MainDetected = false;
+    private bool FirstDetect = true;
+    private bool open = false;
+    private float depla = 0;
+    private float pas = 0.01f;
+    private float delay = 0.05f;
+    private float time_update = 0f;
+    private bool FirstMove = true;
+
+    public bool getOpen() { return open; }
+
+    private void OnTriggerEnter(Collider col)
+    {
+        if ((col.gameObject == mainDroite || col.gameObject == mainGauche) && FirstDetect)
+        {
+            MainDetected = true;
+            FirstDetect = false;
+        }
+    }
+
+    private void Start()
+    {
+        time_update = delay;
+    }
+
+    void Update()
+    {
+        float time = Time.time;
+        if (MainDetected && !open)
+        {
+            //MainDetected = false;
+            if (time > time_update)
+            {
+                deplacement();
+                time_update = Time.time + delay;
+            }
+            if (depla == 0.5f && !FirstMove)
+            {
+                open = true;
+            }
+            
+        }
+    }
+
+    void deplacement()
+    {
+        if (FirstMove)
+        {
+            if (depla < 0.5f)
+            {
+                gameObject.transform.GetChild(0).transform.GetChild(0).transform.localPosition += new Vector3(-pas, 0, pas);
+                gameObject.transform.GetChild(1).transform.GetChild(0).transform.localPosition += new Vector3(-pas, 0, pas);
+                gameObject.transform.GetChild(2).transform.GetChild(0).transform.localPosition += new Vector3(-pas, 0, pas);
+                gameObject.transform.GetChild(3).transform.GetChild(0).transform.localPosition += new Vector3(-pas, 0, pas);
+                depla += pas;
+            }
+            else
+            {
+                FirstMove = false;
+                depla = 0.0f;
+            }
+        }
+        else
+        {
+            if (depla <= 0.5f)
+            {
+                gameObject.transform.GetChild(0).transform.GetChild(1).transform.localPosition += new Vector3(-pas, 0, pas);
+                gameObject.transform.GetChild(1).transform.GetChild(1).transform.localPosition += new Vector3(-pas, 0, pas);
+                gameObject.transform.GetChild(2).transform.GetChild(1).transform.localPosition += new Vector3(-pas, 0, pas);
+                gameObject.transform.GetChild(3).transform.GetChild(1).transform.localPosition += new Vector3(-pas, 0, pas);
+                depla += pas;
+            }
+        }
+
+    }
+
+}
